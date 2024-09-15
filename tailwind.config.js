72fd11db31ee97108be8b970eb8a12bad8c43b02
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-09-15 14:03:51 +0530

    new color added to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       colors: {
         darkGray: '#393646',
+        pureDarkGray:'#111015',
         lightDarkGrey:'#45394D',
         purpleGray: '#4F4557',
         mutedPurple: '#6D5D6E',
